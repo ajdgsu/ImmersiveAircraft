e143402cc6ae7027e7ee271747086846614bb088
ajdgsu <45306865+ajdgsu@users.noreply.github.com>
2024-09-13 03:59:42 +0800

    Update AirplaneEntity.java

--- common/src/main/java/immersive_aircraft/entity/AirplaneEntity.java
+++ common/src/main/java/immersive_aircraft/entity/AirplaneEntity.java
@@ -49,9 +49,9 @@ public abstract class AirplaneEntity extends AircraftEntity {
         Vector3f direction = getForwardDirection();
 
         // speed
-        float thrust = (float) (Math.pow(getEnginePower(), 100000.0) * getProperties().get(VehicleStat.ENGINE_SPEED));
-        if (onGround() && getEngineTarget() < 0.05) {
-            thrust = getProperties().get(VehicleStat.PUSH_SPEED) / (1.0f + (float) getDeltaMovement().length() * 150.0f) * pressingInterpolatedZ.getSmooth() * (1.0f - getEnginePower());
+        float thrust = (float) (Math.pow(getEnginePower(), 1000.0) * getProperties().get(VehicleStat.ENGINE_SPEED));
+        if (onGround() && getEngineTarget() < 0.9) {
+            thrust = getProperties().get(VehicleStat.PUSH_SPEED) / (1.0f + (float) getDeltaMovement().length() * 1500.0f) * pressingInterpolatedZ.getSmooth() * (1.0f - getEnginePower());
         }
 
         // accelerate
